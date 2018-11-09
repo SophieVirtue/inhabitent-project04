e979fcc8484e1030279fab3878f6308dd6d9db53
Sophie Virtue <sophie.virtue@gmail.com>
2018-11-09 13:31:46 -0800

    init

---
+++ themes/inhabitant/js/custom.js
@@ -0,0 +1,15 @@
+//IIFE
+//immediately invoked function expression
+
+(function($){
+
+$(document).ready(function() {
+
+    //my code here for toggling search form open and closed (.focus and .blur)
+
+});
+
+})(jQuery);
+
+
+
