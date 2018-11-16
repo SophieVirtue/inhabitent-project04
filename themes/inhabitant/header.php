33397ac9fd05cac1fe1aca45d7f9c7af0f2f6fa4
Sophie Virtue <sophie.virtue@gmail.com>
2018-11-16 11:39:38 -0800

    Add header style (minue search toggle)

--- themes/inhabitant/header.php
+++ themes/inhabitant/header.php
@@ -22,15 +22,19 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
+				<div class="container-width">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
+				<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<a href="<?php bloginfo( 'url' ); ?>">
+					<img class="header-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-tent.svg" alt="Tent Logo"/></a>
+				</div><!--.site-branding-->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<?php get_search_form(); ?>
 				</nav><!-- #site-navigation -->
+			</div><!-- .container-width -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
--- themes/inhabitant/single-product.php
+++ themes/inhabitant/single-product.php
@@ -56,5 +56,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
