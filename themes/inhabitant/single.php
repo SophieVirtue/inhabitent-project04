d8538c3579bd200e2a42c290f7171eea5fa5c949
Sophie Virtue <sophie.virtue@gmail.com>
2018-11-19 13:50:56 -0800

    Add style to single blog post page, including comments section

--- themes/inhabitant/single.php
+++ themes/inhabitant/single.php
@@ -9,12 +9,22 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<div class="container-width">
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
+		<div class="social-buttons">
+			<button>
+				<p><i class="fab fa-facebook-f fa-sm"></i> Like</p>
+			</button>
+			<button>
+				<p><i class="fab fa-twitter fa-sm"></i> Tweet</p>
+			</button>
+			<button>
+				<p><i class="fab fa-pinterest fa-sm"></i> Pin</p>
+			</button>
+		</div>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
@@ -24,7 +34,7 @@ get_header(); ?>
 			?>
 
 		<?php endwhile; // End of the loop. ?>
-
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
