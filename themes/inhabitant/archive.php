fef44f373fdd2a870d8ba60a06d2977fe121c133
Sophie Virtue <sophie.virtue@gmail.com>
2018-11-19 17:00:53 -0800

    Fix style spacing for all shop, journal, and find-us pages

--- themes/inhabitant/archive.php
+++ themes/inhabitant/archive.php
@@ -3,43 +3,39 @@
  * The template for displaying archive pages.
  *
  * @package RED_Starter_Theme
- * Template Name: Archive Product Term Template
+ * Template Name: Archive Sorted Template
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area container-width">
 		<main id="main" class="site-main" role="main">
-			<div class="container-width archive">
-				<?php if ( have_posts() ) : ?>
 
-				<header class="page-header">
-					<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-					?>
-				</header><!-- .page-header -->
+		<?php if ( have_posts() ) : ?>
 
-				<?php /* Start the Loop */ ?>
-				<div class="product-grid">
-					<?php while ( have_posts() ) : the_post(); ?>
-					<div class="product-item">
-						<div class="thumbnail">
-  							<a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail( 'medium' ); ?></a>
-						</div>
-						<div class="product-info">
-							<p><?php the_title(); ?>
-							<div class="ellipsis"></div>
-							<?php echo '$' . CFS()->get( 'price' ); ?></p>
-						</div>
-					</div>
-					<?php endwhile; ?>
-				</div>
-				<?php else : ?>
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-				<?php endif; ?>
-			</div>
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content' ); ?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-	<?php get_footer(); ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
